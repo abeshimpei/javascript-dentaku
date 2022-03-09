03ce98df897573afd9017ecbbae381054cec7472
EC2 Default User <ec2-user@ip-172-31-10-124.ec2.internal>
2022-03-09 02:44:56 +0000

    電卓課題

---
+++ main.js
@@ -0,0 +1,38 @@
+let result = document.getElementById("result");
+
+function number(target) {
+    if (result.value == "0") {
+      result.value = target.value;
+    } else if (result.value == "00") {
+        result.value = target.value;
+    } else {
+        result.value += target.value;
+    }
+};
+
+function operator(target) {
+    if (result.value.slice(-1) == "+"){
+    return;
+    } else if (result.value.slice(-1) == "-") {
+    return;
+    } else if (result.value.slice(-1) == "*") {
+    return;
+    } else if (result.value.slice(-1) == "/") {
+    return;
+    } else if (result.value.slice(-1) == ".") {
+    return;
+    } else {
+     result.value += target.value;
+    }
+};
+
+function acBtn(){
+result.value = '0';
+}
+
+function equalBtn() {
+   result.value = new Function("return " + result.value)();
+};
+    
+
+
